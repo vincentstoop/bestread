46d57def6d6d6b02868f6bc53d52214ed188c30e
vincentstoop <vincentstoop@gmail.com>
2017-01-13 16:27:04 +0100

    Create Author show page.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,3 +1,24 @@
 class Author < ApplicationRecord
   has_many :books
+
+  # def full_name
+  #   if Author[:prefix] != ""
+  #     full = "#{Author[:first_name]} #{Author[:prefix]} #{Author[:last_name]}"
+  #   else
+  #     full = :first_name + " " + :last_name
+  #   end
+  #   return full
+  # end
+  def full_name
+    first = read_attribute(:first_name)
+    prefix = read_attribute(:prefix)
+    last = read_attribute(:last_name)
+
+    if prefix != ""
+      full = "#{first} #{prefix} #{last}"
+    else
+      full = "#{first} #{last}"
+    end
+  end
+
 end
