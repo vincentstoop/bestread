358eed849da460bdf29a94bed10afe23cc07eaef
njarigsma <nicky.jarigsma@gmail.com>
2017-01-13 22:23:20 +0100

    Create author_edit and create _form for edit and new

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,3 +1,5 @@
+# I don't understand why we can have an instance variable (@author) that changes with every other method. Wasn't the whole point of an instance variable that it would be accessable from outside the method (but inside the class?)
+
 class AuthorsController < ApplicationController
   def index
     @authors = Author.all
@@ -22,7 +24,23 @@ class AuthorsController < ApplicationController
        render 'new'
     end
   end
-  
+
+# I don't understand why we have an edit method and an update method.
+  def edit
+    @author = Author.find(params[:id])
+  end
+
+  def update
+    @author = Author.find(params[:id])
+    author_params = params.require(:author).permit(:first_name, :prefix, :last_name, :author_img, :bio, :quotes)
+
+    if @author.update_attributes(author_params)
+      redirect_to @author
+    else
+      render 'edit'
+    end
+  end
+
   def destroy
     @author = Author.find(params[:id])
     @author.destroy
