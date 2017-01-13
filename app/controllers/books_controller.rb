01b2145a87541fae8d9b44a5a8777672e395e635
cwexp <waes73@gmail.com>
2017-01-13 19:39:22 +0100

    Create add new book (ERROR)

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,6 +4,22 @@ class BooksController < ApplicationController
   end
 
   def show
-    @book = Book.find(params[:id])  
+    @book = Book.find(params[:id])
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    book_params = params.require(:book).permit(:title, :year, :cover_img, :desc, :pages, :isbn)
+
+    @book = Book.new(book_params)
+
+    if @book.save
+       redirect_to @book
+    else
+       render 'new'
+    end
   end
 end
